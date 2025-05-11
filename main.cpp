f85fad5aec1b6555effb97dee4db60733c6d8188
Haerbernd <jan@schmidt-oc.de>
2025-05-11 22:51:52 +0200

    Added Player class and implemented it

--- main.cpp
+++ main.cpp
@@ -1,29 +1,23 @@
+#include <iostream>
 #include <string>
 #include <cstdlib>
 
 class GameCharacter {
     public:
+        static std::string type;
+
         std::string name;
-        int maxHealth;
         int currentHealth;
         int damage;
         bool isAlive;
 
-    GameCharacter(std::string, int, int, int);
     GameCharacter(std::string, int, int);
     void takeDamage(int);
-    void heal(int);
 };
 
-GameCharacter::GameCharacter(std::string _name, int _maxHealth, int _currentHealth, int _damage) {
-    name = _name;
-    maxHealth = _maxHealth;
-    currentHealth = _currentHealth;
-    damage = _damage;
-    isAlive = true;
-}
 
-GameCharacter::GameCharacter(std::string _name, int _health, int _damage): name(_name), maxHealth(_health), currentHealth(_health), damage(_damage) {
+
+GameCharacter::GameCharacter(std::string _name, int _health, int _damage): name(_name), currentHealth(_health), damage(_damage) {
     isAlive = true;
 }
 
@@ -35,19 +29,48 @@ void GameCharacter::takeDamage(int amount) {
     }
 }
 
-void GameCharacter::heal(int amount) {
+void attack(GameCharacter * attacker, GameCharacter * target) {
+    target->takeDamage(attacker->damage);
+}
+
+class Player: public GameCharacter {
+    public:
+        static std::string type;
+
+        int maxHealth;
+        int armor;
+
+    Player(std::string, int, int, int);
+    void heal(int);
+    void takeDamage(int);
+};
+
+Player::Player(std::string _name, int _maxHealth, int _currentHealth, int _damage): GameCharacter(_name, _currentHealth, _damage) {
+    maxHealth = _maxHealth;
+    armor = 5;
+}
+
+void Player::takeDamage(int amount) {
+    if (amount - armor) {
+        return;
+    }
+    currentHealth -= (amount - armor);
+    if (currentHealth <= 0) {
+        currentHealth = 0;
+        isAlive = false;
+    }
+}
+
+
+void Player::heal(int amount) {
     currentHealth += amount;
     if (currentHealth > maxHealth) {
         currentHealth = maxHealth;
     }
 }
 
-
-
-
-
 int main() {
-    GameCharacter alice = GameCharacter("Alice", 100, 100, 10);
+    GameCharacter alice = Player("Alice", 100, 100, 10);
     GameCharacter goblin1 = GameCharacter("Goblin", 50, 5);
     GameCharacter goblin2 ("Goblin", 50, 5);
     GameCharacter goblin3 {"Goblin", 50, 5};
